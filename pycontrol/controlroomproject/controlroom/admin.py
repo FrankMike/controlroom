6ad60545b9d1b41f887d6f1d90dc09b50af4862a
Francesco Micheli <francescomicheli@proton.me>
2024-10-22 15:12:49 +0400

    Add total movies and tv shows count and size on the home page

--- pycontrol/controlroomproject/controlroom/admin.py
+++ pycontrol/controlroomproject/controlroom/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import DiaryEntry
+from .models import DiaryEntry, Transaction
 
 
-# @admin.register(DiaryEntry)
+@admin.register(DiaryEntry)
 class DiaryAdmin(admin.ModelAdmin):
     list_display = ("user", "date", "content")
-    ordering = "date"
+    ordering = ("date",)
     search_fields = ("date", "content")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("user", "description", "amount", "transaction_type", "date")
+    ordering = ("date",)
+    search_fields = ("description", "amount")
--- pycontrol/controlroomproject/controlroom/views.py
+++ pycontrol/controlroomproject/controlroom/views.py
@@ -8,12 +8,29 @@ import logging
 logger = logging.getLogger(__name__)
 
 from .models import DiaryEntry, Transaction
+from media.models import Movie, TVShow
 from .forms import DiaryEntryForm, TransactionForm
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    context = {}
+    if request.user.is_authenticated:
+        total_movies = Movie.objects.count()
+        total_movies_size = sum(movie.file_size_gb for movie in Movie.objects.all())
+        total_tv_shows = TVShow.objects.count()
+        total_tv_shows_size = sum(show.file_size_gb for show in TVShow.objects.all())
+
+        context.update(
+            {
+                "total_movies": total_movies,
+                "total_movies_size": f"{total_movies_size / 1024:.2f} TB",
+                "total_tv_shows": total_tv_shows,
+                "total_tv_shows_size": f"{total_tv_shows_size / 1024:.2f} TB",
+            }
+        )
+
+    return render(request, "home.html", context)
 
 
 @login_required
