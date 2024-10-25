6e08c994a3c23c6de4415fd2d34de7765f156f3d
Francesco Micheli <francescomicheli@proton.me>
2024-10-25 16:50:50 +0400

    Add whitenoise library to manage static files

--- controlroomproject/controlroomproject/settings.py
+++ controlroomproject/controlroomproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os, sys
+import os  # , sys
 
 from dotenv import load_dotenv
 
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -135,22 +136,18 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "controlroom" / "static",
+]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
-STATICFILES_DIRS = [
-    BASE_DIR / "controlroom" / "static",
-]
-
-# This is for production, you can comment it out during development
-STATIC_ROOT = BASE_DIR / "staticfiles"
 
 PLEX_TOKEN = os.getenv("PLEX_TOKEN")
 PLEX_URL = os.getenv("PLEX_URL")
